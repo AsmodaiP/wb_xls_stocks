712c9936e42e93a53721c9bd11ed53ff79f7c2e2
Asmodai <asmoday.p@gmail.com>
2022-03-20 08:51:14 +0300

    working version

--- bot.py
+++ bot.py
@@ -7,10 +7,19 @@ from logging.handlers import RotatingFileHandler
 from dotenv import load_dotenv
 from telegram.ext import (CommandHandler, ConversationHandler, Filters,
                           MessageHandler, Updater)
+from telegram import KeyboardButton, ReplyKeyboardMarkup
 
 import sheet
 
 load_dotenv()
+
+MAIN_MENU = (
+    [KeyboardButton('Получить таблицу'),],)
+
+MAIN_MENU_MARKUP = ReplyKeyboardMarkup(
+    MAIN_MENU,
+    resize_keyboard=True,
+    one_time_keyboard=False)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 log_file = os.path.join(BASE_DIR, 'bot.log')
@@ -42,42 +51,66 @@ def start(bot, update):
         codecs.open(
             'instruction.md',
             'r').read(),
-        parse_mode='Markdown')
+        parse_mode='Markdown', reply_markup=MAIN_MENU_MARKUP)
+
+
+def send_old_table(bot, update):
+    bot.message.reply_text('Старая таблица')
+    bot.message.reply_document(open('stocks.xlsx', 'rb'))
+
+
+def send_new_table(bot, update):
+    bot.message.reply_text('Актуальная таблица')
+    bot.message.reply_document(open('stocks.xlsx', 'rb'))
 
 
 def file_manager(bot, update):
+    '''
+    Используется для обработки файлов и определения того,
+    нужно ли добавить остатки из этого файла,
+    обновить основную табилцу или внести приход
+    '''
+    send_old_table(bot, update)
     file_id = bot.message.document['file_id']
     name = bot.message.document['file_name']
     if name == 'stocks.xlsx':
         update.user_data['file_id'] = file_id
-        bot.message.reply_text('Введите ДА для подтверждения замены главного файла')
+        bot.message.reply_text(
+            'Введите ДА для подтверждения замены главного файла')
         return 'update_main_file_confirmation'
     file = update.bot.get_file(file_id)
     file.download(name)
-    result = list(sheet.update_stocks(name)['erorrs'])
-    bot.message.reply_text('Обновленная таблица')
-    bot.message.reply_document(open('stocks.xlsx', 'rb'))
+    if name == 'поставка.xlsx':
+        update.user_data['file_id'] = file_id
+        sdfsd = sheet.insert_supplie(name)
+        logging.info(sdfsd)
+        result = sheet.insert_supplie(name)['erorrs']
+    else:
+        result = list(sheet.insert_sales(name)['erorrs'])
+    send_new_table(bot, update)
     if result:
-        bot.message.reply_text('Нет этих артикулов в таблице\n'
+        bot.message.reply_text('Этих артикулов  нет в таблице. Скорее всего нужно добавить их и их актуальный остаток в файл и провести замену главного файла\n'
                                +
                                '\n'.join(result), parse_mode='Markdown')
     return ConversationHandler.END
 
+
 def update_main_file(bot, update):
+    '''Обновляет главный файл и отправляет его пользователю'''
     if bot.message.text.lower() != 'да':
         bot.message.reply_text('Замена главного файла отменена')
         return ConversationHandler.END
-
-    bot.message.reply_text('Старая таблица')
-    bot.message.reply_document(open('stocks.xlsx', 'rb'))
 
     file_id = update.user_data['file_id']
     name = 'stocks.xlsx'
     file = update.bot.get_file(file_id)
     file.download(name)
+    send_new_table(bot, update)
+    return ConversationHandler.END
 
 
 def cancel(bot, update):
+    '''Служит стандартным выходом из любого места'''
     bot.message.reply_text('Операция отменена')
     return ConversationHandler.END
 
@@ -88,16 +121,20 @@ start_handler = CommandHandler('start', start)
 updater.dispatcher.add_handler(start_handler)
 
 
-update_main_file_handler = MessageHandler(Filters.text & ~Filters.command, update_main_file)
+update_main_file_handler = MessageHandler(
+    Filters.text & ~Filters.command, update_main_file)
 
 document_handler = MessageHandler(
     Filters.document.file_extension("xlsx"),
     file_manager)
 
+get_table_handler = MessageHandler(Filters.text(['Получить таблицу']), send_new_table)
+updater.dispatcher.add_handler(get_table_handler)
+
 dialog = ConversationHandler(
     entry_points=[document_handler],
     states={
-        'update_main_file_confirmation':  [MessageHandler(Filters.text & ~Filters.command, update_main_file)],
+        'update_main_file_confirmation': [MessageHandler(Filters.text & ~Filters.command, update_main_file)],
     },
     fallbacks=[CommandHandler('cancel', cancel)])
 
--- sheet.py
+++ sheet.py
@@ -15,9 +15,9 @@ def get_counts_from_table(name='db.xlsx'):
     ws = wb.active
     values = []
     for row in ws.rows:
-        
+
         try:
-            
+
             value = str(row[0].value)
             if value.isdigit():
                 values.append(value)
@@ -26,6 +26,7 @@ def get_counts_from_table(name='db.xlsx'):
     cnt = Counter(values)
     return cnt
 
+
 def barcode_in_data(barcode, data):
     if barcode is None or barcode == 'Бакрод':
         return False
@@ -33,17 +34,19 @@ def barcode_in_data(barcode, data):
         return False
     return True
 
+
 def get_mirrors(wb):
 
-    wb.active = len(wb.sheetnames)-1
+    wb.active = len(wb.sheetnames) - 1
     ws = wb.active
     mirrors = []
-    for row in  ws.rows:
+    for row in ws.rows:
         if row[0].value is None:
             continue
         val = row[0].value.replace(' ', '')
         mirrors.append(val.split(','))
     return mirrors
+
 
 def get_all_mirrors(barcode, mirrors):
     for mirors_barcodes in mirrors:
@@ -51,39 +54,48 @@ def get_all_mirrors(barcode, mirrors):
             return set(mirors_barcodes)
     return set([str(barcode)])
 
-def insert_data_in_table(data, target_table='stocks.xlsx'):
+
+def insert_data_in_table(data, first_index,fill, target_table='stocks.xlsx'):
     wb = openpyxl.load_workbook(target_table)
     mirrors = get_mirrors(wb)
-    for list_number in range(len(wb.sheetnames)-1):
+    for list_number in range(len(wb.sheetnames) - 1):
         wb.active = list_number
         ws = wb.active
         for row in ws.rows:
             barcode = row[0].value
             mirrors_barcodes = get_all_mirrors(barcode, mirrors)
-            new_value =   get_sum_by_barcode(mirrors_barcodes, data, row)
+            new_value = get_sum_by_barcode(mirrors_barcodes, data, row)
             if new_value != 0:
-                row[FIRST_INDEX + datetime.now().day].value = new_value
-                row[FIRST_INDEX + datetime.now().day].fill = PatternFill(start_color="B4C6E7", end_color="B4C6E7", fill_type = "solid")
+                row[first_index + datetime.now().day].value = new_value
+                row[first_index + datetime.now().day].fill = fill
         wb.save(target_table)
-    return {'erorrs':data}
+    return {'erorrs': data}
+
 
 def get_sum_by_barcode(mirrors_barcodes, data, row):
     sum_of_mirrors = 0
     for bar in mirrors_barcodes:
-        
+
         if str(bar) in data:
             sum_of_mirrors += data[str(bar)]
             del data[str(bar)]
 
     today_sells = row[FIRST_INDEX + datetime.now().day].value
     if today_sells is not None:
-        return sum([int(today_sells),sum_of_mirrors])
+        return sum([int(today_sells), sum_of_mirrors])
     return sum_of_mirrors
 
 
-def update_stocks(filename):
-    return  insert_data_in_table(get_counts_from_table(filename))
+def insert_sales(filename):
+    fill = PatternFill(start_color="B4C6E7", end_color="B4C6E7", fill_type="solid")
+    return insert_data_in_table(
+        get_counts_from_table(filename), first_index=FIRST_INDEX, fill=fill)
+
+def insert_supplie(filename):
+    fill = PatternFill(start_color="92D050", end_color="92D050", fill_type="solid")
+    return insert_data_in_table(
+        get_counts_from_table(filename), first_index=FIRST_INDEX+33, fill=fill) 
 
 # 2000393474004
 if __name__ == '__main__':
-    insert_data_in_table(get_counts_from_table())
+    pass
